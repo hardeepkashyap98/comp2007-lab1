f30742da807a17fe63b40bf395ff9d98ba68434f
hardeepkashyap98 <hardeepkashyap98@gmail.com>
2018-02-13 21:05:55 -0500

    comp2007-lab1

---
+++ Lab1/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Products/Details?device=[value]
+        public ActionResult Details(string device)
+        {
+            ViewBag.Device = device;
+            return View();
+        }
+
+    }
+}
